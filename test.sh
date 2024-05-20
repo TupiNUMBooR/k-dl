1dab04385592b7d8f701cfcb7a62ee27a4014ee1
TupiNUMBooR <TupiNUMBooR@gmail.com>
2024-05-20 22:58:51 +1000

    Update test

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-./gallery-dl --help
+./gallery-dl --version
 ./gallery-dl https://wallhaven.cc/w/werowr
 ./gallery-dl https://wallhaven.cc/w/werowr https://www.pixiv.net/en/artworks/118901427
 echo test ok
